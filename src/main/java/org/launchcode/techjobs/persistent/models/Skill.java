e82e0d17805b0a5914d2f1fc74105c0ddf7d2574
toby king <tobyjacquesking@gmail.com>
2022-09-29 20:15:15 -0400

    test two passing

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -12,9 +12,25 @@ public class Skill extends AbstractEntity {
     @ManyToMany(mappedBy="skills")
     private List<Job> jobs = new ArrayList<>();
 
+    public Skill(String description, List<Job> jobs) {
+        this.description = description;
+        this.jobs = jobs;
+    }
 
+    public String getDescription() {
+        return description;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public List<Job> getJobs() {
+        return jobs;
+    }
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
 
     public Skill () {}
-}
+    }
